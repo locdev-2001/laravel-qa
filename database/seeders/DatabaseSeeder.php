30e303f339f55704684c73118c952fbee70de091
LocNguyen <theghostz2612@outlook.com>
2024-01-22 17:09:23 +0700

    Prepupulate users and questions tables using model factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Question;
+use App\Models\User;
+use Database\Factories\QuestionFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::factory(5)->create()->each(function ($user){
+            $user->questions()->saveMany(
+                Question::factory(rand(1,5))->make()
+            );
+        });
+//
     }
 }
