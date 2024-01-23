21d890d57e7c242802cd0a1be32b2da9ae7fe37a
LocNguyen <theghostz2612@outlook.com>
2024-01-23 14:58:14 +0700

    Build Question Form

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -22,7 +22,8 @@ class QuestionController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+        return view('questions.create',['question'=>$question]);
     }
 
     /**
